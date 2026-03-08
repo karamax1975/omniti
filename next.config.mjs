faea7a9dde41522391c12991aadfec8ba71ef504
Andrey Volkov <volkov-aa@m13.ru>
2026-03-08 16:25:43 +0300

    Подготовка проекта к деплою

--- next.config.mjs
+++ next.config.mjs
@@ -15,8 +15,8 @@ const nextConfig = {
     images: {
     unoptimized: true,
     },
-    basePath: '/omniti',
-    assetPrefix: '/omniti/',
+    basePath: '/pages_omniti',
+    assetPrefix: '/pages_omniti/',
 };
 
 export default nextConfig;
